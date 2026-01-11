90a26f5942bb4b9fe238e451e565f6b5aa118442
ribhupramanik <ribhu02@gmail.com>
2026-01-11 19:57:04 +0530

    Create the api to ftech user profile

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req,res) => {
+    const {username} = req.params;
+
+    try {
+        const user = await User.findOne({username}).select("-password")
+        if(!user){
+            return res.status(404).json({error: "User not found"})
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in getUserProfile: ", error.message)
+        res.status(500).json({error:error.message});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ app.use(express.urlencoded({ extended:true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
  app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
