af41700cf243fa9ce087b580022895bbfa53632e
ribhupramanik <ribhu02@gmail.com>
2026-01-11 19:01:34 +0530

    Api for user create and user login are setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,6 +21,9 @@ export const signup = async (req, res) => {
             return res.status(400).json({ error: "Email is already taken" });
         }
 
+        if(password.length < 6){
+            return res.status(400).json({ error: "Password must be at least 6 characters long" })
+        }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -55,9 +58,33 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.json({
-        data: "You hit the login endpoint",
-    })
+    try {
+       
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect){
+            return res.status(400).json({ error: "Invalid username or password" })
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        
+        res.status(201).json({
+                _id: user._id,
+                fullName: user.fullName,
+                username: user.username,
+                email: user.email,
+                followers: user.followers,
+                following: user.following,
+                profileImg: user.profileImg,
+                coverImg: user.coverImg
+            })
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 export const logout = async (req, res) => {
