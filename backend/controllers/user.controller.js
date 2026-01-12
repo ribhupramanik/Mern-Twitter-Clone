3239e04212187067e13579a3d545278c4d712046
ribhupramanik <ribhu02@gmail.com>
2026-01-12 17:27:36 +0530

    Setup the api to get suggested users to follow

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req,res) => {
@@ -36,11 +37,44 @@ export const followUnfollowUser = async(req, res) => {
         }else{
             await User.findByIdAndUpdate(id, {$push: { followers: req.user._id } });
             await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+
+            const newNotification = new Notification({
+                type:'follow',
+                from: req.user._id,
+                to: userToModify._id
+            });
+            await newNotification.save()
             res.status(200).json({message: "User followed successfully"});
         }
 
     } catch (error) {
          console.log("Error in followUnfollowUser: ", error.message)
         res.status(500).json({error:error.message});
+    }
+}
+
+export const getSuggestedUsers = async (req, res)=>{
+    try {
+        const userId = req.user._id;
+
+        const usersFollowedByMe = await User.findById(userId).select("following");
+
+        const users = await User.aggregate([
+            {
+                $match:{
+                    _id:{$ne:userId}
+                }
+            },
+            {$sample:{size:10}}
+        ])
+        const filteredUsers = users.filter(user=>!usersFollowedByMe.following.includes(user._id))
+        const suggestedUsers = filteredUsers.slice(0,4)
+
+        suggestedUsers.forEach((user)=>(user.password=null));
+
+        res.status(200).json(suggestedUsers);
+    } catch (error) {
+        console.log("Error in getSuggestedUsers: ", error.message);
+        res.status(500).json({error: error.message});
     }
 }
