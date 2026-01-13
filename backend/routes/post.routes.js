9a279101b21e548f129fd7b9fc9509cd2c22d675
ribhupramanik <ribhu02@gmail.com>
2026-01-13 13:41:56 +0530

    Post delete API works

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost } from "../controllers/post.contoller.js";
+import { createPost, deletePost } from "../controllers/post.contoller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost)
 // router.post("/like/:id", protectRoute, likeUnlikePost)
 // router.post("/comment/:id", protectRoute, commentOnPost)
-// router.delete("/", protectRoute, deletePost)
+router.delete("/:id", protectRoute, deletePost)
 
 export default router;
