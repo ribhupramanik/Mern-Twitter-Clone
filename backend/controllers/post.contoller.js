076ee29d337adc374d7184a2423e4c6456874af8
ribhupramanik <ribhu02@gmail.com>
2026-01-13 15:04:31 +0530

    API to fetch all posts along with the user information is setup

--- backend/controllers/post.contoller.js
+++ backend/controllers/post.contoller.js
@@ -126,3 +126,24 @@ export const likeUnlikePost = async (req, res) => {
     }
 }
 
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({createdAt: -1}).populate({
+            path:"user",
+            select:"-password"
+        }).populate({
+            path:"comments.user",
+            select:"-password"
+        })
+
+        if(posts.len === 0){
+            return res.status(200).json([])
+        }
+        res.status(200).json(posts);
+
+    } catch (error) {
+        console.log("Error in getAllPosts controller: ", error);
+        res.status(500).json({error: "Internal server error"})
+    }
+}
+
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, deletePost, commentOnPost, likeUnlikePost } from "../controllers/post.contoller.js";
+import { createPost, deletePost, commentOnPost, likeUnlikePost, getAllPosts } from "../controllers/post.contoller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts)
 router.post("/create", protectRoute, createPost)
 router.post("/like/:id", protectRoute, likeUnlikePost)
 router.post("/comment/:id", protectRoute, commentOnPost)
