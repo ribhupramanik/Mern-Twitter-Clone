a8d0ed7f9c20af40dd8cb7f392401a3fa617dcd9
ribhupramanik <ribhu02@gmail.com>
2026-01-14 14:29:39 +0530

    API for viewing and deleting notifications are setup

---
+++ backend/routes/notofication.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getNotifications, deleteNotification } from "../controllers/notification.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications)
+router.delete("/",protectRoute, deleteNotification)
+
+export default router;
+
