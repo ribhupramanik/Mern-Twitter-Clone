846c70327faa88e51f06cf7dd9af7d6ec09f458c
ribhupramanik <ribhu02@gmail.com>
2026-01-14 20:30:18 +0530

    Designed the signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage"
+import SignUpPage from './pages/auth/signup/SignUpPage';
 
 function App() {
-  
-
   return (
-    <p>Hello</p>
-  )
+    <div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App
