c05668b5189010b1f04188a89e2383c90a8107c2
ribhupramanik <ribhu02@gmail.com>
2026-01-14 16:34:07 +0530

    Configured daisyui theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,7 @@ function App() {
   
 
   return (
-    <div data-theme="black">
-      <button class="btn btn-neutral">Neutral</button>
-      <button class="btn btn-primary">Primary</button>
-      <button class="btn btn-secondary">Secondary</button>
-      <button class="btn btn-accent">Accent</button>
-      <button class="btn btn-info">Info</button>
-      <button class="btn btn-success">Success</button>
-      <button class="btn btn-warning">Warning</button>
-      <button class="btn btn-error">Error</button>
-    </div>
+    <p>Hello</p>
   )
 }
 
