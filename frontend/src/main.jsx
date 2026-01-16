34dee259a128deb92edce37399c27a39ca16867b
ribhupramanik <ribhu02@gmail.com>
2026-01-16 17:48:19 +0530

    ALlowed users to signup using the signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter }  from "react-router-dom";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider  } from "@tanstack/react-query";
 
 const queryClient = new QueryClient()
 
