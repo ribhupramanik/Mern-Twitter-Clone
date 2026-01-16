5fdbfc57f488609a93217268b46094b5d6e14c1d
ribhupramanik <ribhu02@gmail.com>
2026-01-16 23:30:11 +0530

    Add authentication checks to route elements in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage"
 import SignUpPage from './pages/auth/signup/SignUpPage';
@@ -11,8 +11,9 @@ import { Toaster } from 'react-hot-toast';
 import { useQuery } from '@tanstack/react-query';
 import LoadingSpinner from './components/common/LoadingSpinner';
 
+
 function App() {
-	const { data, isLoading, error, isError } = useQuery({
+	const { data:authUser, isLoading, error, isError } = useQuery({
 		queryKey: ['authUser'],
 		queryFn: async() => {
 			try {
@@ -41,11 +42,11 @@ function App() {
     <div className='flex max-w-6xl mx-auto'>
       <Sidebar/>
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/notifications' element={<NotificationPage/>}/>
-				<Route path='/profile/:username' element={<ProfilePage/>} />
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/"/>} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage/> : <Navigate to="/login" />}/>
+				<Route path='/profile/:username' element={authUser ? <ProfilePage/> : <Navigate to="/login" />} />
 			</Routes>
       <RightPanel/>
 	  <Toaster/>
