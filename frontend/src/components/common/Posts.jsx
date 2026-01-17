c7f4a053adaf826bcb434600f823ab29e22b2edd
ribhupramanik <ribhu02@gmail.com>
2026-01-17 11:02:38 +0530

    Refactor Posts component to fetch posts based on feedType and handle loading state

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,40 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({feedType}) => {
+
+	const getPostEndPoint = () => {
+		switch (feedType){
+			case "forYou":
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			default:
+				return "/api/posts/all"
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndPoint();
+
+	const {data:posts, isLoading}= useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+
+				return data;
+
+			}catch(error){
+				throw new Error(error);
+			}
+		},
+	})
 
 	return (
 		<>
@@ -14,10 +45,10 @@ const Posts = () => {
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && posts && (
 				<div>
-					{POSTS.map((post) => (
+					{posts.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
 				</div>
