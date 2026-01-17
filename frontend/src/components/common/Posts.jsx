a0489eb048fdc770eb2c95c08bd9235f7318afd4
ribhupramanik <ribhu02@gmail.com>
2026-01-17 11:32:52 +0530

    Pass feedType prop to Posts component and refactor loading state handling

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,6 +1,7 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const Posts = ({feedType}) => {
 
@@ -17,7 +18,7 @@ const Posts = ({feedType}) => {
 
 	const POST_ENDPOINT = getPostEndPoint();
 
-	const {data:posts, isLoading}= useQuery({
+	const {data:posts, isLoading, refetch, isRefetching}= useQuery({
 		queryKey: ["posts"],
 		queryFn: async () => {
 			try {
@@ -36,17 +37,21 @@ const Posts = ({feedType}) => {
 		},
 	})
 
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch])
+
 	return (
 		<>
-			{isLoading && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && posts && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
 					{posts.map((post) => (
 						<Post key={post._id} post={post} />
