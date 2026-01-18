da433c8c25dffac4b168738ab22920ca54bf36b3
ribhupramanik <ribhu02@gmail.com>
2026-01-18 22:23:40 +0530

    Implement comment functionality with loading state and user feedback

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -66,6 +66,36 @@ const Post = ({ post }) => {
     },
   });
 
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: comment }),
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Sommething went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+		toast.success("Comment posted successfully");
+		setComment("");
+		queryClient.invalidateQueries({queryKey: ['posts']});
+	},
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   const postOwner = post.user;
   const isLiked = post.likes.includes(authUser._id);
 
@@ -73,14 +103,14 @@ const Post = ({ post }) => {
 
   const formattedDate = "1h";
 
-  const isCommenting = false;
-
   const handleDeletePost = () => {
     deletePost();
   };
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if (isCommenting) return;
+    commentPost();
   };
 
   const handleLikePost = () => {
